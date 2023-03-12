bc07e9b613820c8a540af80c5c3631f1e1fc229f
Tolgonai Andakulova <tolgonaia@gmail.com>
2023-03-12 23:36:51 +0600

    fix bug in full cocktail info

--- src/components/FullCocktailInfo.tsx
+++ src/components/FullCocktailInfo.tsx
@@ -6,10 +6,26 @@ interface FullCocktailProps {
 }
 
 const FullCocktailInfo = ({cocktail}: FullCocktailProps) => {
+
+  const isIng = (str: string) => {
+    for (let i = 1; i <= 15; i++) {
+      if(str === 'strIngredient'+i) {
+        return `${str}`
+      }
+    }
+  }
+
   return (
     <div className='flex'>
-      <img src={cocktail.strDrinkThumb ? cocktail.strDrinkThumb : ''}/>
-
+      <img src={cocktail.strDrinkThumb ? cocktail.strDrinkThumb : ''} alt={cocktail.strDrink ? cocktail.strDrink : ''}/>
+      <div>
+        <h3>{cocktail.strDrink}</h3>
+        <p>Type: {cocktail.strAlcoholic}</p>
+        <p>Category: {cocktail.strCategory}</p>
+        {Object.keys(cocktail).filter(isIng).map((el, index) => {
+          let key = 'strIngredient'+index
+          return <p>{cocktail[key]}</p>})}
+      </div>
     </div>
   );
 };
